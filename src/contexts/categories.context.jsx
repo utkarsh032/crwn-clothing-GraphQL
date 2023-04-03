dc6a58a6b67f8000ef6913b8b203f0ce9a89586c
utkarsh032 <utkarshhraj525@gmail.com>
2023-04-03 09:10:01 +0530

    Adding GraphQL +Apollo Client

---
+++ src/contexts/categories.context.jsx
@@ -0,0 +1,46 @@
+import { createContext, useState, useEffect } from 'react'
+
+import { gql, useQuery } from '@apollo/client'
+
+export const CategoriesContext = createContext({
+  categoriesMap: {}
+})
+
+const COLLECTIONS = gql`
+  query {
+    collections {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`
+
+export const CategoriesProvider = ({ children }) => {
+  const { loading, error, data } = useQuery(COLLECTIONS)
+  const [categoriesMap, setCategoriesMap] = useState({})
+
+  useEffect(() => {
+    if (data) {
+      const { collections } = data
+      const collectionsMap = collections.reduce((acc, collections) => {
+        const { title, items } = collections
+        acc[title.toLowerCase()] = items
+        return acc
+      }, {})
+      setCategoriesMap(collectionsMap)
+    }
+  }, [data])
+
+  const value = { categoriesMap,loading }
+  return (
+    <CategoriesContext.Provider value={value}>
+      {children}
+    </CategoriesContext.Provider>
+  )
+}
